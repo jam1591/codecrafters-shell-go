f37412af65a21b0a9b551f76fbffdea1aff85d61
jam1591 <75530123+jam1591@users.noreply.github.com>
2026-06-24 20:47:53 +0100

    codecrafters submit [skip ci]

--- app/commandFactory.go
+++ app/commandFactory.go
@@ -53,14 +53,7 @@ func parseTokens(rawCmd string) []string {
 
 	for curr < len(rawCmd) {
 		switch rawCmd[curr] {
-		case '"':
-			curr += 1
-			temp := curr
-			for ; curr < len(rawCmd) && rawCmd[curr] != '"'; curr += 1 {
-			}
-			currToken += rawCmd[temp:curr]
-			curr += 1
-		case '\'':
+		case '\'', '"':
 			curr += 1
 			temp := curr
 			for ; curr < len(rawCmd) && rawCmd[curr] != '\''; curr += 1 {
