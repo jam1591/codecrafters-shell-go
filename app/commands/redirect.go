6c6fa7136106d819910a31b2780b68bfb3d94ffc
Vaidotas Petraitis <vaidotas.petraitis8@gnmail.com>
2026-07-15 22:00:43 +0100

    codecrafters submit [skip ci]

--- app/commands/redirect.go
+++ app/commands/redirect.go
@@ -1,9 +1,6 @@
 package commands
 
-import (
-	"fmt"
-	"os"
-)
+import "os"
 
 const (
 	OVERRIDE = os.O_CREATE | os.O_WRONLY | os.O_TRUNC
@@ -26,23 +23,26 @@ func (c *RedirectCommand) Execute() {
 	if c.Metadata.FilePathStdout != "" {
 		file, err := os.OpenFile(c.Metadata.FilePathStdout, getFlagForRedirect(c.Metadata.IsAppend), 0643)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\r\n", c.Metadata.FilePathStdout, err)
-			return
+			panic(err)
 		}
 		defer file.Close()
 		tempOut := os.Stdout
-		defer func() { os.Stdout = tempOut }()
+		defer func() {
+			os.Stdout = tempOut
+		}()
 		os.Stdout = file
 	}
+
 	if c.Metadata.FilePathStderr != "" {
 		file, err := os.OpenFile(c.Metadata.FilePathStderr, getFlagForRedirect(c.Metadata.IsAppend), 0643)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\r\n", c.Metadata.FilePathStderr, err)
-			return
+			panic(err)
 		}
 		defer file.Close()
 		tempErr := os.Stderr
-		defer func() { os.Stderr = tempErr }()
+		defer func() {
+			os.Stderr = tempErr
+		}()
 		os.Stderr = file
 	}
 
--- app/main.go
+++ app/main.go
@@ -22,56 +22,60 @@ const (
 const BUILT_INS = "echo, exit, type, pwd, cd"
 
 func main() {
-	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("$ ")
 
-		oldState, err := term.MakeRaw(fd)
-		if err != nil {
-			panic(err)
-		}
+		command, _ := reader.ReadString('\n')
+		// var command []byte
 
-		var command []byte
-	completion:
-		for {
-			b, err := reader.ReadByte()
-			if err != nil {
-				term.Restore(fd, oldState)
-				fmt.Fprintln(os.Stderr, "Error reading input:", err)
-				os.Exit(1)
-			}
-			builtInsArray := strings.Split(BUILT_INS, ", ")
-			switch b {
-			case 127, 8:
-				fmt.Print("\b \b")
-				continue
-			case 13:
-				fmt.Print("\r\n")
-				break completion
-			case 9:
-				partialCmd := strings.TrimSpace(string(command))
-				var matches = make([]string, 0)
-				for _, cmd := range builtInsArray {
-					if strings.HasPrefix(cmd, partialCmd) {
-						matches = append(matches, cmd)
-					}
-					if len(matches) == 1 {
-						command = []byte(matches[0])
-						command = append(command, ' ')
-						fmt.Printf("\r$ %s", string(command))
-					} else if len(matches) > 1 {
-						fmt.Printf("\r\n%v\r\n$ %s", matches, string(command))
-					}
-				}
-				continue
-			}
-			command = append(command, b)
-			fmt.Printf("%c", b)
-		}
+		// completion:
+		// 	for {
+		// 		b, err := reader.ReadByte()
+		// 		if err != nil {
+		// 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		// 			os.Exit(1)
+		// 		}
 
-		term.Restore(fd, oldState)
+		// 		builtInsArray := strings.Split(BUILT_INS, ", ")
+
+		// 		switch b {
+		// 		case 127, 8:
+		// 			fmt.Print("\b \b")
+		// 			continue
+		// 		case 13:
+		// 			fmt.Println()
+		// 			break completion
+		// 		case 9:
+		// 			partialCmd := strings.TrimSpace(string(command))
+		// 			var matches = make([]string, 0)
+		// 			for _, cmd := range builtInsArray {
+		// 				if strings.HasPrefix(cmd, partialCmd) {
+		// 					matches = append(matches, cmd)
+		// 				}
+
+		// 				if len(matches) == 1 {
+		// 					command = []byte(matches[0])
+		// 					command = append(command, ' ')
+		// 					fmt.Printf("\r$ %s", string(command))
+		// 				} else if len(matches) > 1 {
+		// 					fmt.Printf("\r\n%v\r\n$ %s", matches, string(command))
+		// 				}
+		// 			}
+
+		// 			continue
+		// 		}
+
+		// 	command = append(command, b)
+		// 	fmt.Printf("%c", b)
+		// }
 
 		commandFactory := &CommandFactory{parser: &Parser{}}
 		executor := commandFactory.NewCommand(strings.TrimSpace(string(command)))
