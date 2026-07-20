71b8d6d5f33400e67cb71cc4cb57d3100589d73d
Vaidotas Petraitis <vaidotas.petraitis8@gnmail.com>
2026-07-20 19:51:47 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -31,37 +31,29 @@ type Completer struct {
 
 func (b *Completer) Do(line []rune, pos int) (newLine [][]rune, length int) {
 	matches, length := b.completer.Do(line, pos)
-	fmt.Fprintf(os.Stderr, "\n[DEBUG] Do called, matches=%d, isLastBellAmbiguous=%v\n", len(matches), b.state.isLastBellAmbiguous)
 
-	if len(matches) == 0 {
+	if len(matches) > 1 {
+		sort.Slice(matches, func(i, j int) bool {
+			return string(matches[i]) < string(matches[j])
+		})
+	}
+
+	switch len(matches) {
+	case 0:
 		fmt.Fprint(os.Stderr, "\a")
 		b.state.isLastBellAmbiguous = false
-		return matches, length
-	}
 
-	if len(matches) == 1 {
+	case 1:
 		b.state.isLastBellAmbiguous = false
-		return matches, length
+
+	default: // more than 1 match
+		if !b.state.isLastBellAmbiguous {
+			fmt.Fprint(os.Stderr, "\a")
+			b.state.isLastBellAmbiguous = true
+		} else {
+			b.state.isLastBellAmbiguous = false
+		}
 	}
-
-	if !b.state.isLastBellAmbiguous {
-		fmt.Fprint(os.Stderr, "\a")
-		b.state.isLastBellAmbiguous = true
-		fmt.Fprintln(os.Stderr, "[DEBUG] first tab, beeped")
-		return matches, length
-	}
-
-	sorted := make([]string, len(matches))
-	for i, m := range matches {
-		sorted[i] = string(m)
-	}
-	sort.Strings(sorted)
-	fmt.Fprintf(os.Stderr, "[DEBUG] second tab, raw matches=%q sorted=%q\n", matches, sorted)
-
-	fmt.Println()
-	fmt.Println(strings.Join(sorted, "  "))
-
-	b.state.isLastBellAmbiguous = false
 
 	return matches, length
 }
